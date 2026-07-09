de72a27886dbdeb34e0cfcb6079e032adfba670e
Larry <git@mail.lambda-larry.net>
2026-07-09 12:46:28 +0000

    feat(pp): Add support for lc_rune to pretty printing

--- gdb/pretty-print.py
+++ gdb/pretty-print.py
@@ -15,6 +15,16 @@ class lc_sv(gdb.ValuePrinter):
         len = int(self.__val['length'])
         return self.__val['s'].string(length=len)
 
+class lc_rune(gdb.ValuePrinter):
+    def __init__(self, val):
+        self.__val = val
+
+    def to_string(self):
+        codepoint = int(self.__val)
+        return f"'{chr(codepoint)}'"
+
 pp.add_printer('lc_sv', '^lc_sv$', lc_sv)
+pp.add_printer('lc_rune', '^lc_rune$', lc_rune)
+
 
 gdb.printing.register_pretty_printer(gdb.current_objfile(), pp, replace=True)
