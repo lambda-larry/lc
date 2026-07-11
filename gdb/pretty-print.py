4bdcfdedc7cb546e766e18c3682624b6c8ed978c
Larry <git@mail.lambda-larry.net>
2026-07-11 16:34:24 +0000

    feat(pp): Add support for lc_clock for pretty printing

--- gdb/pretty-print.py
+++ gdb/pretty-print.py
@@ -23,8 +23,21 @@ class lc_rune(gdb.ValuePrinter):
         codepoint = int(self.__val)
         return f"'{chr(codepoint)}'"
 
+
+class lc_clock(gdb.ValuePrinter):
+    def __init__(self, val):
+        self.__val = val
+
+    def to_string(self):
+        sec  = int(self.__val['sec'])
+        nsec = int(self.__val['nsec'])
+        return f"{sec}.{nsec:09}s"
+
+
+
 pp.add_printer('lc_sv', '^lc_sv$', lc_sv)
 pp.add_printer('lc_rune', '^lc_rune$', lc_rune)
+pp.add_printer('lc_clock', '^lc_clock$', lc_clock)
 
 
 gdb.printing.register_pretty_printer(gdb.current_objfile(), pp, replace=True)
