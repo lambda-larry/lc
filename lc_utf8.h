14d91c66d96d2971518bedb32ebde0a6c0bdc563
Larry <git@mail.lambda-larry.net>
2026-07-19 09:06:52 +0000

    chore(utf8): Expand unicode cheatsheet

--- lc_utf8.h
+++ lc_utf8.h
@@ -34,6 +34,8 @@ lc_utf8_decode(size_t *restrict n, const char *restrict *restrict in, lc_rune *r
 
 	lc_rune cp = 0;
 
+	/// Cheat sheet:
+	///
 	/// Code point <-> UTF-8 conversion
 	/// First code point | Last code point | Byte 1   | Byte 2   | Byte 3   | Byte 4
 	/// -----------------+-----------------+----------+----------+----------+---------
@@ -41,6 +43,33 @@ lc_utf8_decode(size_t *restrict n, const char *restrict *restrict in, lc_rune *r
 	/// U+0080           | U+07FF          | 110xxxyy | 10yyzzzz |          |
 	/// U+0800           | U+FFFF          | 1110wwww | 10xxxxyy | 10yyzzzz |
 	/// U+010000         | U+10FFFF        | 11110uvv | 10vvwwww | 10xxxxyy | 10yyzzzz
+	///
+	/// Bit-pattern mask | Lower bound | Upper bound
+	/// -----------------+-------------+------------
+	/// 0yyyzzzz         | 0x00        | 0x7F
+	/// 110xxxyy         | 0xC0        | 0xDF
+	/// 1110wwww         | 0xE0        | 0xEF
+	/// 11110uvv         | 0xF0        | 0xF7
+	///
+	/// ID | Plane             | Name                                      | Blocks | Characters
+	/// ---+-------------------+-------------------------------------------+--------+------
+	/// 0  | 0x000000-0x00FFFF | Basic Multilingual Plane (BMP)            | 164    | 55718
+	/// 1  | 0x010000-0x01FFFF | Supplementary Multilingual Plane (SMP)    | 168    | 28869
+	/// 2  | 0x020000-0x02FFFF | Supplementary Ideographic Plane (SIP)     | 7      | 61513
+	/// 3  | 0x030000-0x03FFFF | Tertiary Ideographic Plane (TIP)          | 3      | 13429
+	/// 4  | 0x040000-0x04FFFF | Unassigned                                | 0      | 0
+	/// 5  | 0x050000-0x05FFFF | Unassigned                                | 0      | 0
+	/// 6  | 0x060000-0x06FFFF | Unassigned                                | 0      | 0
+	/// 7  | 0x070000-0x07FFFF | Unassigned                                | 0      | 0
+	/// 8  | 0x080000-0x08FFFF | Unassigned                                | 0      | 0
+	/// 9  | 0x090000-0x09FFFF | Unassigned                                | 0      | 0
+	/// 10 | 0x0A0000-0x0AFFFF | Unassigned                                | 0      | 0
+	/// 11 | 0x0B0000-0x0BFFFF | Unassigned                                | 0      | 0
+	/// 12 | 0x0C0000-0x0CFFFF | Unassigned                                | 0      | 0
+	/// 13 | 0x0D0000-0x0DFFFF | Unassigned                                | 0      | 0
+	/// 14 | 0x0E0000-0x0EFFFF | Supplementary Special-purpose Plane (SSP) | 2      | 337
+	/// 15 | 0x0F0000-0x0FFFFF | Supplementary Private Use Area-A (SPUA-A) | 1      | 0
+	/// 16 | 0x100000-0x10FFFF | Supplementary Private Use Area-B (SPUA-B) | 1      | 0
 
 	if (IN_RANGE(*p, 0x00, 0x7F)) {
 		cp = (cp << 6) | (0x7F & *p++);
